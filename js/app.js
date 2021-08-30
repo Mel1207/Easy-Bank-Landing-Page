7e9d6b880d2a6314c85b66a8a5b57e2096d701d7
Mel <meljohnpualon@gmail.com>
2021-08-31 04:13:55 +0800

    added toggle class for mobile menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const hamburger = document.querySelector('.hamburger');
+const mobileMenuContainer = document.querySelector('.menu-mobile-container')
+
+const toggleMenu = (e) => {
+    mobileMenuContainer.classList.toggle('open')
+}
+
+hamburger.addEventListener('click', toggleMenu)
