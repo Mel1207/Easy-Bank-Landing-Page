4eb7b5bc66481deebe5f95685ce5a8919aa7bf4a
Mel <meljohnpualon@gmail.com>
2021-08-31 04:19:28 +0800

    fix body and menu toggle classes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const hamburger = document.querySelector('.hamburger');
-const mobileMenuContainer = document.querySelector('.menu-mobile-container')
+const mobileMenuContainer = document.querySelector('.menu-mobile-container');
+const body = document.querySelector('body')
 
 const toggleMenu = (e) => {
     mobileMenuContainer.classList.toggle('open')
+    body.classList.toggle('overflow-hide')
 }
 
 hamburger.addEventListener('click', toggleMenu)
